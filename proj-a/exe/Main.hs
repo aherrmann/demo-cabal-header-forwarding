9a8fd35de2e30e275129ccc9335fd08fd47cfc83
Andreas Herrmann <andreas.herrmann@tweag.io>
2018-12-20 18:55:39 +0100

    initial commit

---
+++ proj-a/exe/Main.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE CPP #-}
+
+module Main where
+
+#include "header.h"
+
+main :: IO ()
+main = print A
---
+++ proj-a/lib/Lib.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE CPP #-}
+
+module Lib where
+
+#include "header.h"
+
+foo :: Int
+foo = A
